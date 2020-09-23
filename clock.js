79933890de4ed5447fc7a69cb340861d91583cb3
haeun1049 <haeun1049@gmail.com>
2020-09-23 17:59:00 +0900

    200923_haeun

--- clock.js
+++ clock.js
@@ -1,14 +1,19 @@
 const clockContainer = document.querySelector(".js-clock"),
       clockTitle = clockContainer.querySelector("h1");
+      clockMain = clockContainer.querySelector("h2");
 
     function getTime() {
         const date = new Date();
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;  // 월
+        const day = date.getDate(); 
+
         const minutes = date.getMinutes();
         const hours = date.getHours();
         const seconds = date.getSeconds();
+        clockMain.innerText = `${year}년 ${month}월 ${day}일`
         clockTitle.innerText = 
         `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
-
     }
 
       function init() {
